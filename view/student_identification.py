98a5ab7dc63ff97a1de706b7a3115212422b15c4
quillaur <aurelienquillet@gmail.com>
2022-08-04 14:26:50 +0200

    Modify id form to have columns.

--- view/student_identification.py
+++ view/student_identification.py
@@ -3,9 +3,14 @@ import streamlit as st
 def set_identification_form(title_holder: st.empty, main_holder: st.empty, available_classes: list) -> None:
     title_holder.title("Identification form")
     with main_holder.form("Identification"):
-        firstname = st.text_input("Firstname:")
-        lastname = st.text_input("Lastname:")
-        your_class = st.text_input("Your class:")
+        col1, col2, col3 = st.columns(3)
+        with col1:
+            firstname  = st.text_input("Firstname:")
+        with col2:
+            lastname = st.text_input("Lastname:")
+        with col3:
+            your_class = st.text_input("Your class:")
+        
         selected_study = st.selectbox("Please select what you would like to study:", available_classes)
 
         submitted = st.form_submit_button("Submit")
