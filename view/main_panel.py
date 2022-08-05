bc5315b54f68120a7cdd6070256bcd159e2e9450
quillaur <aurelienquillet@gmail.com>
2022-08-05 14:21:41 +0200

    Change global layout and keep working on basic python study.

--- view/main_panel.py
+++ view/main_panel.py
@@ -8,57 +8,62 @@ def set_main_view(main_holder: st.empty) -> None:
     this_page = st.session_state["content"][st.session_state["support_number"]]
 
     with main_holder.container():
-        st.title(this_page["title"])
 
-        # 3 possible types of supports:
-        if this_page["text"]:
-            st.info(this_page["text"])
-        
-        if this_page["video_url"]:
-            st.video(this_page["video_url"])
+        for k, v in this_page.items():
+            if v:
+                if k == "title":
+                    st.title(v)
 
-        if this_page["image"]:
-            img_path = join(st.session_state["study_path"], this_page["image"])
-            st.image(Image.open(img_path))
+                # 3 possible types of supports:        
+                elif k == "video_url":
+                    print(v)
+                    st.video(v)
 
-        # Is there a question to ask ?
-        if this_page["question"]:
-            st.write(this_page["question"])
+                elif k == "image":
+                    img_path = join(st.session_state["study_path"], v)
+                    st.image(Image.open(img_path))
+                
+                elif k == "text":
+                    st.info(v)
 
-            if st.session_state["pages_done"][st.session_state["support_number"]]:
-                st.warning(f"This answer was: {this_page['answer']}")
-            else:
-                resp = st.selectbox("Your answer:", options=this_page["possible answers"])
-                submit = st.button("Submit")
-            
-                if submit:
-                    if resp == this_page["answer"]:
-                        st.success("Good job ! This is correct.")
-                        st.session_state["score"] += 1
-                        if st.session_state["delta"] >= 0:
-                            st.session_state["delta"] += 1
-                        else:
-                            st.session_state["delta"] = 1
+                # Is there a question to ask ?
+                elif k == "question":
+                    st.write(v)
+
+                    if st.session_state["pages_done"][st.session_state["support_number"]]:
+                        st.warning(f"This answer was: {this_page['answer']}")
                     else:
-                        st.error(f"Sorry... The answer was: {this_page['answer']}")
-                        st.session_state["score"] -= 1
-
-                        if st.session_state["delta"] <= 0:
-                            st.session_state["delta"] -= 1
-                        else:
-                            st.session_state["delta"] = -1
+                        resp = st.selectbox("Your answer:", options=this_page["possible answers"])
+                        submit = st.button("Submit")
                     
-                    st.session_state["max_score"] += 1
-                    st.session_state["pages_done"][st.session_state["support_number"]] = True
-        
-        if "certif_ratio" in this_page and this_page["certif_ratio"]:
-            st.header(f"Your final score: {st.session_state['score']} / {st.session_state['max_score']}")
+                        if submit:
+                            if resp == this_page["answer"]:
+                                st.success("Good job ! This is correct.")
+                                st.session_state["score"] += 1
+                                if st.session_state["delta"] >= 0:
+                                    st.session_state["delta"] += 1
+                                else:
+                                    st.session_state["delta"] = 1
+                            else:
+                                st.error(f"Sorry... The answer was: {this_page['answer']}")
+                                st.session_state["score"] -= 1
 
-            score_ratio = (st.session_state["score"] / st.session_state["max_score"]) * 100
+                                if st.session_state["delta"] <= 0:
+                                    st.session_state["delta"] -= 1
+                                else:
+                                    st.session_state["delta"] = -1
+                            
+                            st.session_state["max_score"] += 1
+                            st.session_state["pages_done"][st.session_state["support_number"]] = True
+                
+                elif k == "certif_ratio":
+                    st.header(f"Your final score: {st.session_state['score']} / {st.session_state['max_score']}")
 
-            if score_ratio > this_page["certif_ratio"]:
-                st.success("Well done! You can get this study certification !")
-            else:
-                st.error("Sorry, you did not score high enough to get this certification.")
-                st.info("Feel free to retry anytime !")
+                    score_ratio = (st.session_state["score"] / st.session_state["max_score"]) * 100
+
+                    if score_ratio > v:
+                        st.success("Well done! You can get this study certification !")
+                    else:
+                        st.error("Sorry, you did not score high enough to get this certification.")
+                        st.info("Feel free to retry anytime !")
                 
