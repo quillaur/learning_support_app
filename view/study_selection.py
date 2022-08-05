56f638e7c1411c9b49636b52831cc4d06cdc1b7d
quillaur <aurelienquillet@gmail.com>
2022-08-05 09:38:39 +0200

    Add English and French possibility.

--- view/study_selection.py
+++ view/study_selection.py
@@ -1,3 +1,4 @@
+import json
 import streamlit as st
 from os import listdir
 from os.path import join
@@ -11,10 +12,21 @@ def select_study_view(main_holder: st.empty):
         # What studies are available ?
         col1, col2 = st.columns(2)
         with col1:
-            selected_category = st.selectbox("Filter by category:", listdir("classes/"))
-            category_path = join("classes", selected_category)
+            selected_language = st.selectbox("Language:", ["English", "Français"])
         with col2:
-            selected_study = st.selectbox("Please select what you would like to study:", listdir(category_path))
+            selected_category = st.selectbox("Category:", listdir("classes/"))
+            category_path = join("classes", selected_category)
+
+        filtered_studies = []
+        for s in listdir(category_path):
+            config_path = join(category_path, s, "config.json")
+            with open(config_path, "r") as json_file:
+                json_content = json.load(json_file)
+            
+            if json_content["language"] == selected_language:
+                filtered_studies.append(s)
+
+        selected_study = st.selectbox("Please select what you would like to study:", filtered_studies)
         
         select = st.button("Select")
     
