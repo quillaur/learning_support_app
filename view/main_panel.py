91f86cef84c550716b888bff07cb63ce07089d07
quillaur <aurelienquillet@gmail.com>
2022-08-09 15:47:44 +0200

    Draw the certification dynamicaly.

--- view/main_panel.py
+++ view/main_panel.py
@@ -3,6 +3,7 @@ from os.path import join
 from PIL import Image, ImageDraw, ImageFont
 from matplotlib import font_manager
 from zipfile import ZipFile
+from math import ceil
 
 def update_delta(positif: bool) -> None:
     """
@@ -37,7 +38,51 @@ def update_score(positif: bool) -> None:
 
 
     
+def draw_certificat_info(img: Image) -> None:
+    # Call draw Method to add 2D graphics in an image
+    I1 = ImageDraw.Draw(img)
+    # Custom font style and font size
+    font = font_manager.FontProperties(family='sans-serif', weight='bold')
+    file = font_manager.findfont(font)
+    font_size = 50
+    myFont = ImageFont.truetype(file, font_size)
+    txt = f"{st.session_state['firstname']} {st.session_state['lastname']}"
+    # Adjust font size until it fits the box:
+    while myFont.getsize(txt)[0] > 400:
+        font_size -= 1
+        myFont = ImageFont.truetype(file, font_size)
 
+    # Add name of student to certificat
+    color = (255, 255, 255)
+    I1.text((615,210), txt, font=myFont, fill=color)
+
+    # Add study name
+    study_name = " ".join(st.session_state['selected_study'].split("_"))
+    I1.text((615,365), study_name.capitalize(), font=myFont, fill=color)
+
+    # Add score
+    score_ratio = ceil((st.session_state["score"] / st.session_state["max_score"]) * 100) if st.session_state["max_score"] > 0 else 0
+    I1.text((115,290), f"Success: {score_ratio}%", font=myFont, fill=color)
+
+
+def draw_image(image_name: str) -> Image:
+    # Images are all compressed in a zip file names 'ressources.zip'.
+    resc_path = join(st.session_state["study_path"], "ressources.zip")
+    with ZipFile(resc_path, "r") as myzip:
+        with myzip.open(f"ressources/{image_name}") as img_file:
+            img = Image.open(img_file)
+    
+            if "certificat" in image_name:
+                draw_certificat_info(img)
+    
+    return img
+
+
+@st.cache()
+def prep_certif_for_download(img: Image) -> bytes:
+    img_name ="prepared_certification.png"
+    img.save(img_name)
+    return img_name
 
 
 def set_main_view(main_holder: st.empty) -> None:
@@ -70,26 +115,9 @@ def set_main_view(main_holder: st.empty) -> None:
 
                 # There can be several image keys.
                 elif k.startswith("image"):
-                    # Images are all compressed in a zip file names 'ressources.zip'.
-                    resc_path = join(st.session_state["study_path"], "ressources.zip")
-                    with ZipFile(resc_path, "r") as myzip:
-                        with myzip.open(f"ressources/{v}") as img_file:
-                            img = Image.open(img_file)
-                    
-                            if "certificat" in v:
-                                # Call draw Method to add 2D graphics in an image
-                                I1 = ImageDraw.Draw(img)
-                                # Custom font style and font size
-                                font = font_manager.FontProperties(family='sans-serif', weight='bold')
-                                file = font_manager.findfont(font)
-                                myFont = ImageFont.truetype(file, 50)
-                                # Add Text to an image
-                                I1.text((620,200), f"{st.session_state['firstname']} {st.session_state['lastname']}", font=myFont, fill=(255, 0, 0))
-
-                                study_name = " ".join(st.session_state['selected_study'].split("_"))
-                                I1.text((620,360), study_name.capitalize(), font=myFont, fill=(255, 0, 0))
-                            
-                            st.image(img)
+                    if not "certificat" in v:
+                        img = draw_image(v)
+                        st.image(img)
                 
                 # There can be several text keys.
                 elif k.startswith("text"):
@@ -103,14 +131,14 @@ def set_main_view(main_holder: st.empty) -> None:
                     # just show the answer of the question. 
                     # Make it impossible for the user to answer several times to the same question.
                     if st.session_state["pages_done"][st.session_state["support_number"]]:
-                        st.warning(f"This answer was: {this_page['answer']}")
+                        st.warning(f"The answer was: {this_page['answer']}")
                     else:
                         # Show the possible answers in a multiselect fassion.
-                        resp = st.multiselect("Your answer:", options=this_page["possible answers"])
+                        resp = st.multiselect("Your answer:", options=this_page["possible answers"], default=None)
 
                         submit = st.button("Submit")
                         if submit:
-                            good_answer: bool = resp == this_page["answer"]
+                            good_answer: bool = resp.sort() == this_page["answer"].sort()
                             update_score(good_answer)
                             update_delta(good_answer)
 
@@ -127,12 +155,30 @@ def set_main_view(main_holder: st.empty) -> None:
                     score_ratio = (st.session_state["score"] / st.session_state["max_score"]) * 100 if st.session_state["max_score"] > 0 else 0
 
                     # When this is the last page, you see if you can get a certification for this study or not.
-                    st.header(f"Your final score: {st.session_state['score']} / {st.session_state['max_score']} ({score_ratio}%)")
+                    score_msg = f"Your final score: {st.session_state['score']} / {st.session_state['max_score']} ({score_ratio}%)"
 
                     if score_ratio > v:
                         st.balloons()
                         st.success("Well done! You scored high enough to get this study certification !")
+
+                        img = draw_image(this_page["image"])
+                        img_name = prep_certif_for_download(img)
+
+                        col1, col2 = st.columns([4,1])
+                        with col1:
+                            st.warning("Don't forget to download it !")   
+                        with col2:          
+                            with open(img_name, "rb") as bfile:
+                                st.download_button(
+                                    label="Download", 
+                                    data=bfile,
+                                    file_name=img_name,
+                                    mime="image/png"
+                                    )
+                        
+                        st.image(img)
                     else:
-                        st.error("Sorry, you did not score high enough to get this certification.")
+                        st.error(score_msg)
+                        st.warning("Sorry, you did not score high enough to get this certification.")
                         st.info("Feel free to retry anytime !")
                 
