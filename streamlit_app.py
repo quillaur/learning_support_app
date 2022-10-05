2e4a4fe86445b186ce8cd184e88e2a1a47bd02e7
quillaur <aurelienquillet@gmail.com>
2022-10-05 21:11:35 +0200

    Update study and study selection layout.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,11 @@ from view.study_selection import select_study_view
 # Set browser tab name and potential
 st.set_page_config("Learning app", layout="wide")
 
+# st.write("-----------")
+
 select_study_view()
+
+# st.write("-----------")
 
 if "study_path" in st.session_state.keys():
     set_main_view()
--- view/study_selection.py
+++ view/study_selection.py
@@ -7,31 +7,15 @@ def select_study_view():
     """
     This function defines the layout of the page to select a study in the app.
     """
-    # center title
-    _, col2, _ = st.columns(3)
-    with col2:
-        st.title("Select your Study")
-
-    # What studies are available ?
-    col1, col2, col3 = st.columns(3, gap="large")
-    with col1:
-        # Show a welcomy image
-        # st.image("view/love_to_learn_and_teach.jpg")
-        # Filter by language first.    
-        selected_language = st.selectbox("Language:", [d for d in listdir("classes/") if isdir(join("classes/", d))])
-        language_path = join("classes", selected_language)
-        
-    with col2:            
-        # Then filter by category.
-        selected_category = st.selectbox("Category:", [d for d in listdir(language_path) if isdir(join(language_path, d))])
-        category_path = join(language_path, selected_category)
     
-    with col3:
-        # After applying filters, show which study can be selected.
-        selected_study = st.selectbox("Study:", [d for d in listdir(category_path) if isdir(join(category_path, d))])
-        # The select button determine when to save the results in the session state object.
+    st.sidebar.title("Select your Study")
     
-    select = st.button("Select")
+    selected_language = st.sidebar.selectbox("Language:", [d for d in listdir("classes/") if isdir(join("classes/", d))])
+    language_path = join("classes", selected_language)
+    selected_category = st.sidebar.selectbox("Category:", [d for d in listdir(language_path) if isdir(join(language_path, d))])
+    category_path = join(language_path, selected_category)
+    selected_study = st.sidebar.selectbox("Study:", [d for d in listdir(category_path) if isdir(join(category_path, d))])
+    select = st.sidebar.button("Select")
 
     if select:
         st.session_state["selected_study"] = selected_study
